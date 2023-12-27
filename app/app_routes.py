06d7dae99f2617762fb20f34ee0ee22d1a7fc6d2
Suraj Mhatre <surajmcodes@gmail.com>
2023-12-27 10:13:52 +0530

    feat: update operation added for employee

--- app/app_routes.py
+++ app/app_routes.py
@@ -34,6 +34,15 @@ def create_employee(employee: schemas.EmployeeBase, manager_id: int, db: Session
         raise HTTPException(status_code=400, detail="Employee alreadt exists")
     return utils.create_employees(db=db, employees=employee, manager_id=manager_id)
 
+@router.put("/employee/{employee_id}")
+def update_employee(employee_id:int, employee: schemas.EmployeeBase, db: Session = Depends(get_db)):
+    emp = db.query(models.Employee).filter(models.Employee.id == employee_id)
+    if not emp.first():
+        raise HTTPException(status_code=404, detail='Employee not found')
+    emp.update(employee.dict())
+    db.commit()
+    return {"Message":"Employee updated successfully"}
+
 @router.get("/employee/{employee_id}", response_model=schemas.Employee)
 def get_single_employee(employee_id: int, db: Session = Depends(get_db)):
     single_employee = db.query(models.Employee).filter(models.Employee.id == employee_id).first()
