e4e7ef8f5fb77338b67cd6ad6631e92a0675f22d
Suraj Mhatre <surajmcodes@gmail.com>
2023-12-30 05:04:49 +0530

    feat: added api title, description and added cors middleware for security

--- app/main.py
+++ app/main.py
@@ -1,15 +1,33 @@
-
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 import auth_routes
 import managers_routes
 import employees_routes
 
 # creating FastAPI object
-app = FastAPI()
+app = FastAPI(
+    title = "Employee Management API",
+    description = "Helps you manage employee details at your company.",
+    version="0.0.1",
+    contact={
+        "name": "Suraj Mhatre",
+        "url": "https://github.com/suraj-py",
+        "email": "surajrmhatre99@gmail.com",
+    },
+    )
+origins = ["*"]
 
-@app.get("/home", tags=["Hello World"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["Root"])
 async def home():
-    return {"Message":"Hello World"}
+    return {"Message":"Welcome Employee Management API"}
 
 app.include_router(auth_routes.router)
 app.include_router(managers_routes.router)
