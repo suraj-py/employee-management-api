3e8219b272b15347d67e151d386e43694ca5b35a
Suraj Mhatre <surajmcodes@gmail.com>
2024-01-02 02:02:04 +0530

    refactor: changed database url env variable name

--- app/database.py
+++ app/database.py
@@ -7,12 +7,8 @@ import os
 
 load_dotenv()
 
-db_user = os.getenv("DB_USER")
-db_pass = os.getenv("DB_PASS")
-db_host = os.getenv("DB_HOST")
-db_name = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}/{db_name}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # create postgresql database instance
 engine = create_engine(DATABASE_URL)
